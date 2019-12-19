665ae74efe191e91f4c465cf3f67aeb1e52025ac
Stefano-Gallo <stefano_gallo@studenti.naba.it>
2019-12-19 11:04:54 +0100

    Update TexScript

--- End_of_Me/Assets/endofme/Script/ShowUI.cs
+++ End_of_Me/Assets/endofme/Script/ShowUI.cs
@@ -1,28 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShowUI : MonoBehaviour
 {
-
-    public GameObject uiObject;
+    
+    public TMP_Text uiObject;
+    public string text;
     void Start()
     {
-        uiObject.SetActive(false);
+        uiObject.gameObject.SetActive(false);
     }
     // Update is called once per frame
     void OnTriggerEnter(Collider player)
     {
         if (player.gameObject.tag == "Player")
         {
-            uiObject.SetActive(true);
+            uiObject.text = text;
+            uiObject.gameObject.SetActive(true);
             StartCoroutine("WaitForSec");
         }
     }
     IEnumerator WaitForSec()
     {
         yield return new WaitForSeconds(5);
-        uiObject.SetActive(false);
+        uiObject.gameObject.SetActive(false);
         
     }
 
