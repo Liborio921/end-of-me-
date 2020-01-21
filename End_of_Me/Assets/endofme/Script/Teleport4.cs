8970783a86fdd766c7d7011bfa25299ae40dafc6
Maurodicarlo93 <48363516+Maurodicarlo93@users.noreply.github.com>
2020-01-21 19:17:43 +0100

    prova
    prova

---
+++ End_of_Me/Assets/endofme/Script/Teleport4.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport4 : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
