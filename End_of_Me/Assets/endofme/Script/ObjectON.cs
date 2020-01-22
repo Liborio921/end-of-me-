95de2b14f340995f68de3c6097e19fee8a1644cc
ElisaPassera <elisapassera@users.noreply.github.com>
2020-01-22 13:44:12 +0100

    ok

---
+++ End_of_Me/Assets/endofme/Script/ObjectON.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectON : MonoBehaviour
+{
+    public GameObject Off;
+    public GameObject On;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Off.SetActive(false);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            On.SetActive(true);
+        }
+    }
+}
