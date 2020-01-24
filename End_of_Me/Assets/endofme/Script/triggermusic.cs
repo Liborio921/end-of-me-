ca1485ace119a64f23d8ac54f61b2850afd279da
liborio <liboriodagostino8@live.it>
2020-01-24 16:10:49 +0100

    LUCI

---
+++ End_of_Me/Assets/endofme/Script/triggermusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggermusic : MonoBehaviour
+{
+    AudioSource audio;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnTriggerEnter () {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+        Debug.Log("play");
+
+    }
+}
