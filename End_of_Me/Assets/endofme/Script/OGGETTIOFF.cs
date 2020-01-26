31ba5c307cbda3a86d4fd74f4d5d3a80146c2f99
Maurodicarlo93 <48363516+Maurodicarlo93@users.noreply.github.com>
2020-01-26 18:12:38 +0100

    fine

---
+++ End_of_Me/Assets/endofme/Script/OGGETTIOFF.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OGGETTIOFF : MonoBehaviour
+{
+    public GameObject Uno;
+    public GameObject Due;
+    public GameObject Tre;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Uno.SetActive(false);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Due.SetActive(false);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Tre.SetActive(false);
+        }
+    }
+}
---
+++ End_of_Me/Assets/endofme/Script/OGGETTION.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OGGETTION : MonoBehaviour
+{
+    
+    public GameObject On;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            On.SetActive(true);
+        }
+    }
+}
