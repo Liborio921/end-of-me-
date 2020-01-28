8a73179ad70789199397a5af27a64a82abb399a6
Stefano-Gallo <stefano_gallo@studenti.naba.it>
2020-01-28 13:31:50 +0100

    add script and UI

---
+++ End_of_Me/Assets/Example/SelectionController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectionController : MonoBehaviour
+{
+
+    public Material materialIn;
+    public Material materialOut;
+    public Material materialDown;
+
+    public TMP_Text messageUI;
+
+    public string messageIn;
+    public string messageOut;
+    public string messageClick;
+
+    private MeshRenderer _renderer;
+    
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+    
+    private void OnMouseEnter()
+    {
+        _renderer.material = materialIn;
+        messageUI.text = messageIn;
+    }
+
+    private void OnMouseExit()
+    {
+        _renderer.material = materialOut;
+        messageUI.text = messageOut;
+    }
+
+    private void OnMouseDown()
+    {
+        _renderer.material = materialDown;
+        messageUI.text = messageClick;
+    }
+
+    private void OnMouseUp()
+    {
+        _renderer.material = materialIn;
+    }
+}
