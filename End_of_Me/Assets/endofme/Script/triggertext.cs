50c7cfb3be9edc68c8d40e1299461f7502801777
liborio <liboriodagostino8@live.it>
2020-02-01 18:01:36 +0100

    story

---
+++ End_of_Me/Assets/endofme/Script/triggertext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggertext : MonoBehaviour
+{
+    public GameObject Canvas;
+    void Start()
+    {
+        Canvas.SetActive(false);
+
+    }
+    void OnTriggerEnter(Collider Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            Canvas.SetActive(true);
+           
+        }
+
+    }
+
+    void OnTriggerExit(Collider Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            Canvas.SetActive(false);
+        }
+    }
+}
